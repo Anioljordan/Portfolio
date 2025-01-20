914def7e3765a48cd734bda6a4bfe3c1cec45ac3
Aniol <jordan.lopez.aniol@alumnat.copernic.cat>
2025-01-20 17:21:58 +0100

    adding Diplomas

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 function openMenu() {
-    const nav = document.querySelector('nav');
-    nav.classList.toggle('open');
+  const nav = document.querySelector("nav");
+  nav.classList.toggle("open");
+}
+function copyEmail() {
+  const email = "anioljolo@gmail.com";
+  navigator.clipboard
+    .writeText(email)
+    .then(() => {
+      const successMessage = document.getElementById("success-message");
+      successMessage.style.display = "block";
+      successMessage.style.animation = "fadeIn 1s ease-out";
+
+      setTimeout(() => {
+        successMessage.style.display = "none";
+      }, 3000); // El mensaje desaparece después de 3 segundos
+    })
+    .catch((err) => {
+      alert("Error al copiar el correo: " + err);
+    });
 }
